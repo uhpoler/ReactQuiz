d6aca3c4cb14d6354a4acb28a008532167808d69
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-27 21:12:27 +0300

    Added startscreen

---
+++ src/StartScreen.js
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>X question to test your React mastery</h3>
+      <button>Let's start</button>
+    </div>
+  );
+}
