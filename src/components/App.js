03df514e1385bcaf322753c50055da68b7d92c7b
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-28 16:33:53 +0300

    Addded restarting a quiz

--- src/components/App.js
+++ src/components/App.js
@@ -45,6 +45,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
     default:
       throw new Error("Unknown action");
   }
@@ -103,6 +105,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
